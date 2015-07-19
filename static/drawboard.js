778d7206e20d2d16b78d09c32f50eb53987e9fe8
Adam Chalmers <adam.s.chalmers@gmail.com>
2015-07-19 12:09:46 +0700

    Improved mobile layout.

--- static/drawboard.js
+++ static/drawboard.js
@@ -1,6 +1,6 @@
-TILE_SIZE = 50;
+TILE_SIZE = 55;
 CANVAS_SCALE = 2;
-OFFSET = 10;
+OFFSET = 5;
 HL_SIZE = 5;
 
 /*
@@ -8,7 +8,7 @@ HL_SIZE = 5;
  */
 function drawBoard(board) {
     ctx.fillStyle = "#aaa";
-    ctx.fillRect(0, 0, CANVAS_SCALE*(TILE_SIZE*8 + OFFSET*2), CANVAS_SCALE*(TILE_SIZE*8 + OFFSET*2));
+    ctx.fillRect(0, 0, 900, 900);
     for (var i = 0; i < 8; i++) {
         for (var j = 0; j < 8; j++) {
             drawCell(i, j, board[i][j].substring(1), board[i][j].substring(0,1) == "w");
@@ -50,7 +50,7 @@ function drawCell(i, j, text, color, leaveHighlight) {
     ctx.fillText(
         text, 
         CANVAS_SCALE*(OFFSET + i*TILE_SIZE + 15), 
-        CANVAS_SCALE*(4*OFFSET + j*TILE_SIZE)
+        CANVAS_SCALE*(8*OFFSET + j*TILE_SIZE)
     );
   }
 }
